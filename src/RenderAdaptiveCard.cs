ead4b630d89846d0c8021e581889c1bc1e5dc48b
golobitch <tadej.golobic@codingmonkeys.net>
2021-09-18 13:52:56 +0200

    feat(error): add some error handling

--- src/RenderAdaptiveCard.cs
+++ src/RenderAdaptiveCard.cs
@@ -14,6 +14,7 @@ using AdaptiveCards.Rendering.Html;
 using AdaptiveCards.Templating;
 using RenderAdaptiveCard.Resources;
 using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace RenderAdaptiveCard
 {
@@ -23,18 +24,37 @@ namespace RenderAdaptiveCard
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log) {
             log.LogInformation("got request to render card");
 
+            ModelStateDictionary modelState = new ModelStateDictionary();
+
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             
             CardPayloadInput cardPayloadInput = JsonConvert.DeserializeObject<CardPayloadInput>(requestBody);
+            if (cardPayloadInput.Card == null)
+            {
+                modelState.AddModelError("card", "Card field is required");
+                return new BadRequestObjectResult(modelState);
+            }
 
             AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
             
             StringValues svQueryHostConfig;
             if (req.Query.TryGetValue("hostConfig", out svQueryHostConfig))
             {
-                renderer.HostConfig = AdaptiveHostConfig.FromJson(
-                    EmbeddedResourceJsonBase.GetJsonFromEmbeddedResource(string.Format("RenderAdaptiveCard.Resources.HostConfigs.{0}.json", svQueryHostConfig.ToString()))
-                );
+                try
+                {
+                    renderer.HostConfig = AdaptiveHostConfig.FromJson(
+                        EmbeddedResourceJsonBase.GetJsonFromEmbeddedResource(string.Format("RenderAdaptiveCard.Resources.HostConfigs.{0}.json", svQueryHostConfig.ToString()))
+                    );
+                }catch(ArgumentNullException ex)
+                {
+                    modelState.AddModelError("hostConfig", "provided host config does not exists");
+                    return new BadRequestObjectResult(modelState);
+                }
+                catch(Exception ex)
+                {
+                    modelState.AddModelError("hostConfig", ex.Message);
+                    return new BadRequestObjectResult(modelState);
+                }
             }
 
             ICard cardCreator = CardFactory.Create(cardPayloadInput);
